b26736f031be07efa2f25ffa0f7d28d0460d544c
sajid_shaikh <sajid.shaikh@brainvire.com>
2024-01-31 11:04:55 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Koltin_Flow_Demo"
+include(":app")
